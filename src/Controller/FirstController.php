155aee93a3b42634dd3a940779fb48d08e558b98
Barkhoum <90609076+Barkhoum@users.noreply.github.com>
2022-01-21 00:11:05 +0100

    session

---
+++ src/Controller/FirstController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FirstController extends AbstractController
+{
+    #[Route('/first', name: 'first')]
+    public function index(): Response
+    {
+        return $this->render('first/index.html.twig',[
+            'name'=> 'Guesmia',
+            'firstname'=> 'Barkhoum'
+        ]);
+    }
+    #[Route('/sayHello', name: 'say.hello')]
+    public function sayHello(): Response
+    {
+        $rand = rand(3, 10);
+        echo $rand;
+        if($rand % 2 == 0 ){
+            return $this->redirectToRoute(route: 'first');
+        }
+        return $this->forward('App\Controller\FirstController::index',[]);
+
+    }
+}
