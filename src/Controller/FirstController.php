2c99d89360fe5179665f35ad86c7ce20eb70e313
Barkhoum <90609076+Barkhoum@users.noreply.github.com>
2022-01-25 00:04:23 +0100

    fin du cours 14 le routeur de symfony

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -9,6 +9,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FirstController extends AbstractController
 {
+    #[Route('/order/{maVar}', name: 'test.order.route')]
+    public function testOrderRoute($maVar){
+    return new Response("<html><body>$maVar</body><html>");
+    }
+
     #[Route('/first', name: 'first')]//route c'est antislash puis le nom que l'on souhaite afficher puis le name correspont à l'ID de la class!
     public function index(): Response
     {
@@ -23,11 +28,20 @@ class FirstController extends AbstractController
     #[Route('/sayHello/{name}/{firstname}', name:'say.hello')]
     public function sayHello(Request $request, $name, $firstname): Response
     {
-        dd($request);
+
        return $this->render(view: 'first/hello.html.twig', parameters: [
            'nom' => $name,
            'prenom' => $firstname
         ]);
 
+    }
+    #[Route(
+        'multi/{entier1}/{entier2}',// version courte requirement ajouter le regex {entier1<\d+>}
+        name: 'multiplication',
+        requirements: ['entier1'=> '\d+', 'entier2' => '\d+']
+    )]
+    public function multiplication($entier1, $entier2){
+        $resultat = $entier1 * $entier2;
+        return new Response( content: "<h1>Le resultat de la multiplication est de $resultat</h1>");
     }
 }
--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -3,13 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route("/todo")]
 class TodoController extends AbstractController
 {
-    #[Route('/todo', name: 'todo')]
+    /**
+     * @Route("/", name="todo")
+     */
     public function index(Request $request): Response
     {
         $session = $request->getSession();
@@ -29,8 +32,11 @@ class TodoController extends AbstractController
         //si j'ai mon tableau de todo dans ma session je ne fait que l'afficher
         return $this->render(view:'todo/index.html.twig');
     }
-    #[Route('/todo/add/{name}/{content}', name:'todo.add')]
-    public function addTodo(Request $request, $name, $content){
+    #[Route(
+        '/add/{name?test}/{content?test}',// syntaxe courte pour mettre des valeurs par defaut
+        name:'todo.add',
+        defaults:['content' => 'valeur par défault', 'content => contenu par défaut'] )]//syntaxe perso valeurs par défaut
+    public function addTodo(Request $request, $name, $content): RedirectResponse{
         $session = $request->getSession();
         //Verifier si j'ai mon tableau de todo dans la session
         if($session->has(name:'todos')){
@@ -51,6 +57,61 @@ class TodoController extends AbstractController
         //affiche une erreur et on va rediriger vers le controlleur index
         $this->addFlash(type:'error', message:"La liste des todos n'est pas encore initialiséé");
         }
+        return $this->redirectToRoute(route:'todo');
+    }
+    #[Route('/update/{name}/{content}', name:'todo.update')]
+    public function updateTodo(Request $request, $name, $content):RedirectResponse{
+        $session = $request->getSession();
+        //Verifier si j'ai mon tableau de todo dans la session
+        if($session->has(name:'todos')){
+            //si oui
+            // Verifier si on a deja un todo avec le meme name
+            $todos = $session->get(name:'todos');
+            if (!isset($todos[$name])){
+                //si oui afficher le message d'erreur
+                $this->addFlash(type:'danger', message:"le todo avec le $name n'existe déja dans la liste");
+            }else{
+                //Si non on l'ajoute et en affiche un message de succés
+                $todos[$name] = $content;
+                $session->set('todos', $todos);
+                $this->addFlash(type:'success', message:"le todo d'id $name à été modifié avec success");
+
+            }
+        }else{
+            //si non
+            //affiche une erreur et on va rediriger vers le controlleur index
+            $this->addFlash(type:'error', message:"La liste des todos n'est pas encore initialiséé");
+        }
+        return $this->redirectToRoute(route:'todo');
+    }
+    #[Route('/delete/{name}', name:'todo.delete')]
+    public function deleteTodo(Request $request, $name):RedirectResponse{
+        $session = $request->getSession();
+        //Verifier si j'ai mon tableau de todo dans la session
+        if($session->has(name:'todos')){
+            //si oui
+            // Verifier si on a deja un todo avec le meme name
+            $todos = $session->get(name:'todos');
+            if (!isset($todos[$name])){
+                //si oui afficher le message d'erreur
+                $this->addFlash(type:'danger', message:"le todo avec le $name existe déja dans la liste");
+            }else{
+                //Si non on l'ajoute et en affiche un message de succés
+                unset($todos[$name]);
+                $session->set('todos', $todos);
+                $this->addFlash(type:'success', message:"le todo d'id $name à été supprimé avec success");
+            }
+        }else{
+            //si non
+            //affiche une erreur et on va rediriger vers le controlleur index
+            $this->addFlash(type:'error', message:"La liste des todos n'est pas encore initialiséé");
+        }
+        return $this->redirectToRoute(route:'todo');
+    }
+    #[Route('/reset', name:'todo.reset')]
+    public function resetTodo(Request $request):RedirectResponse{
+        $session = $request->getSession();
+        $session->remove(name:'todos');
         return $this->redirectToRoute(route:'todo');
     }
 }
