7711a65a8d4765f0bfafbb91a1dcfe0a26b4eb82
Barkhoum <90609076+Barkhoum@users.noreply.github.com>
2022-01-24 15:50:39 +0100

    session

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -30,26 +30,27 @@ class TodoController extends AbstractController
         //si j'ai mon tableau de todo dans ma session je ne fait que l'afficher
         return $this->render(view:'todo/index.html.twig');
     }
-    #[Route('/todo/{name}/{content}', name:todo.add)]
+    #[Route('/todo/add/{name}/{content}', name:'todo.add')]
     public function addTodo(Request $request, $name, $content){
         $session = $request->getSession();
-        //Vérifier si j'ai mon tableau de todo dans la session
+        //Verifier si j'ai mon tableau de todo dans la session
         if($session->has(name:'todos')){
             //si oui
-            // Vérifier si on a déja un todo avec le meme name
-            $todos =[$session->get(name:'todos')]
-            if ($isset($todos[$name])){
+            // Verifier si on a deja un todo avec le meme name
+            $todos = $session->get(name:'todos');
+            if (isset($todos[$name])){
                 //si oui afficher le message d'erreur
-                $this->addFlash(type:'error', message:"le todo d'id $name existe déja")
+                $this->addFlash(type:'error', message:"le todo d'id $name existe déja");
             }else{
                 //Si non on l'ajoute et en affiche un message de succés
                 $todos[$name] = $content;
-                $this->addFlash(type:'succes', message:"le todo d'id $name à été ajouté avec succès")
+                $this->addFlash(type:'success', message:"le todo d'id $name à été ajouté avec success");
+
             }
         }else{
         //si non
         //affiche une erreur et on va rediriger vers le controlleur index
-        $this->addFlash(type:'error', message:"La liste des todos n'est pas encore initialiséé")
+        $this->addFlash(type:'error', message:"La liste des todos n'est pas encore initialiséé");
         }
         return $this->redirectToRoute(route:'todo');
     }
