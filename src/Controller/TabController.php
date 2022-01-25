cba06e82b7d657f9c126629ff0438187baa6d08f
Barkhoum <90609076+Barkhoum@users.noreply.github.com>
2022-01-25 22:44:08 +0100

    fin twig cours 18

--- src/Controller/TabController.php
+++ src/Controller/TabController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class TabController extends AbstractController
 {
@@ -19,6 +20,19 @@ class TabController extends AbstractController
         //
         return $this->render('tab/index.html.twig', [
             'notes' => $notes,
+        ]);
+    }
+    #[Route('/tab/users', name: 'tab.users')]
+    public function users(): Response
+    {
+        $users = [
+            ['firstname' =>'Barkhoum', 'name'=>'Guesmia', 'age'=>'40'],
+            ['firstname' =>'Tatiana', 'name'=>'Picard', 'age'=>'27'],
+            ['firstname' =>'Pascale', 'name'=>'Moreau', 'age'=>'43']
+        ];
+
+        return $this->render('tab/users.html.twig', [
+            'users' => $users,
         ]);
     }
 }
